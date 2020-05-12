1f4c5202ed0d9a70f2bccc3a0166300424a56bfd
Maciej Samborski <macsamba@gmail.com>
2020-05-12 02:59:08 +0200

    Finished integration

--- SprzedawaczIntegration/SprzedawaczIntegration.cs
+++ SprzedawaczIntegration/SprzedawaczIntegration.cs
@@ -7,16 +7,21 @@ using AngleSharp.Html.Parser;
 using System.Net.Http;
 using AngleSharp.Dom;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Text.Encodings;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SprzedawaczIntegration
 {
     public class SprzedawaczIntegration : IWebSiteIntegration
     {
-        const string mainUrl = "https://sprzedawacz.pl/nieruchomosci/";
-        const string sellFlatsPath = "mieszkania/sprzedaz/";
-        const string rentFlatsPath = "mieszkania/wynajem/";
-        const string sellHousesPath = "domy/sprzedaz/";
-        const string rentHousesPath = "domy/wynajem/";
+        const string mainUrl = "https://sprzedawacz.pl";
+
+        const string sellFlatsPath = "/nieruchomosci/mieszkania/sprzedaz/";
+        const string rentFlatsPath = "/nieruchomosci/mieszkania/wynajem/";
+        const string sellHousesPath = "/nieruchomosci/domy/sprzedaz/";
+        const string rentHousesPath = "/nieruchomosci/domy/wynajem/";
         // "Ilość prezentowanych ogłoszeń została ograniczona do 100 pierwszych podstron.
         // Jeśli chcesz przejrzeć pozostałe ogłoszenia skorzystaj z wyszukiwarki, filtrów
         // lub ogranicz lokalizację."
@@ -35,7 +40,7 @@ namespace SprzedawaczIntegration
             EntriesComparer = equalityComparer;
             WebPage = new WebPage
             {
-                Url = "https://sprzedawacz.pl",
+                Url = mainUrl,
                 Name = "Sprzedawacz.pl Integration",
                 WebPageFeatures = new WebPageFeatures
                 {
@@ -51,14 +56,29 @@ namespace SprzedawaczIntegration
 
         public Dump GenerateDump()
         {
-            var categories = new List<string>
+            List<Entry> entries = new List<Entry>();
+
+            var rentalFlats = GetCategoryUrls($"{mainUrl}{rentFlatsPath}");
+            foreach(string url in rentalFlats)
             {
-                $"{mainUrl}{rentFlatsPath}",
-                $"{mainUrl}{rentHousesPath}",
-                $"{mainUrl}{sellFlatsPath}",
-                $"{mainUrl}{sellHousesPath}"
-            };
-            var entries = new List<Entry>();
+                entries.Add(GetEntryFromUrl($"{mainUrl}{url}", OfferKind.RENTAL));
+            }
+            var saleFlats = GetCategoryUrls($"{mainUrl}{sellFlatsPath}");
+            foreach (string url in saleFlats)
+            {
+                entries.Add(GetEntryFromUrl($"{mainUrl}{url}", OfferKind.SALE));
+            }
+            var rentalHouses = GetCategoryUrls($"{mainUrl}{rentHousesPath}");
+            foreach (string url in rentalHouses)
+            {
+                entries.Add(GetEntryFromUrl($"{mainUrl}{url}", OfferKind.RENTAL, true));
+            }
+            var saleHouses = GetCategoryUrls($"{mainUrl}{sellHousesPath}");
+            foreach (string url in saleHouses)
+            {
+                entries.Add(GetEntryFromUrl($"{mainUrl}{url}", OfferKind.SALE, true));
+            }
+
             return new Dump
             {
                 DateTime = DateTime.Now,
@@ -67,11 +87,121 @@ namespace SprzedawaczIntegration
             };
         }
 
-        private async Task<IDocument> GetParsedHtmlFromUrl(string url)
+        private List<string> GetCategoryUrls(string categoryUrl)
         {
-            var config = Configuration.Default;
+            var urls = new List<string>();
+            var firstPage = GetParsedHtmlFromUrl(categoryUrl);
+            var pagesNumber = int.Parse(firstPage.QuerySelector(".paging").LastElementChild.Text());
+            if (pagesNumber > maxPages)
+                pagesNumber = maxPages;
+            for (int i = 0; i <= pagesNumber; i++)
+            {
+                var listPage = GetParsedHtmlFromUrl($"{categoryUrl}?strona={i}");
+                var offers = listPage.QuerySelectorAll(".ann-row .title a");
+                foreach(IElement offer in offers)
+                {
+                    urls.Add(offer.Attributes.First(a => a.Name == "href").Value);
+                }
+            }
+            return urls;
+        }
+
+        private Entry GetEntryFromUrl(string url, OfferKind kind, bool house = false)
+        {
+            var offerToParse = GetParsedHtmlFromUrl(url);
+            var paragraphs = offerToParse.QuerySelectorAll(".c p");
+            var floor = MatchOrNull(TryGetParagraphValueString(paragraphs, "Piętro:"), "([0-9]+|Parter)")?.Value;
+            floor = floor == "Parter" ? "0" : floor;
+            PolishCity city;
+            var description = offerToParse.QuerySelector(".c .col-md-9 > p").Text();
+            return new Entry
+            {
+                OfferDetails = new OfferDetails
+                {
+                    Url = url,
+                    CreationDateTime = DateTime.Now,
+                    OfferKind = kind,
+                    SellerContact = new SellerContact
+                    {
+                        //Not a proper email validation but it's simple
+                        Email = MatchOrNull(description, @"\w+@\w+\.\w+")?.Value,
+                        Telephone = GetParsedHtmlFromUrl(
+                                $"{mainUrl}/ajax_daj_telefon.php?hash={offerToParse.QuerySelector(".tel-btn")?.GetAttribute("data-hash")}&token={offerToParse.QuerySelector(".tel-btn")?.GetAttribute("data-token")}")
+                            ?.QuerySelector("a")
+                            ?.Text() ?? MatchOrNull(description, "([0-9]{3}(-| )?){2}[0-9]{3}")?.Value,
+                        Name = offerToParse.QuerySelector(".ann-panel-right > p > b").Text()
+                    },
+                    IsStillValid = true
+                },
+                PropertyAddress = new PropertyAddress
+                {
+                    City = Enum.TryParse(MatchOrNull(url, "m_(?<city>[a-z_]+)[-/]").Groups["city"]?.Value.ToUpper(), out city) ? city : default,
+                    District = TryGetParagraphValueString(paragraphs, "Dzielnica:"),
+                    StreetName = MatchOrNull(description, "(ul.|Ul.|UL.) [A-Za-z]+")?.Value,
+                    DetailedAddress = MatchOrNull(description, "(ul. |Ul. |UL. )[A-Za-z]+ (?<detail>[0-9]{1,4}/?[0-9]{1,4})").Groups["detail"]?.Value
+                },
+                PropertyDetails = new PropertyDetails
+                {
+                    Area = house ? TryGetParagraphValueDecimal(paragraphs, "Pow. domu:")
+                        : TryGetParagraphValueDecimal(paragraphs, "Powierzchnia:"),
+                    FloorNumber = floor != null ? int.Parse(floor) : default,
+                    NumberOfRooms = TryGetParagraphValueInt(paragraphs, "Liczba pokoi:"),
+                    YearOfConstruction = TryGetParagraphValueInt(paragraphs, "Rok budowy:")
+                },
+                PropertyFeatures = new PropertyFeatures
+                {
+                    Balconies = Regex.IsMatch(description, "(B|b)alkony?") ? 1 : default,
+                    BasementArea = null,
+                    GardenArea = TryGetParagraphValueDecimal(paragraphs, "Pow. działki:"),
+                    IndoorParkingPlaces = null,
+                    OutdoorParkingPlaces = null
+                },
+                PropertyPrice = new PropertyPrice
+                {
+                    PricePerMeter = TryGetParagraphValueDecimal(paragraphs, "Cena za m2:"),
+                    ResidentalRent = null,
+                    TotalGrossPrice = decimal.Parse(Regex.Matches(offerToParse.QuerySelector(".c > .r > div.text-right > h2").Text(), "[0-9 ]+").Select(m => m.Value).Aggregate((i,j) => i+j).Replace(" ",""))
+                },
+                RawDescription = description
+            };
+        }
+
+        private string TryGetParagraphValueString(IHtmlCollection<IElement> paragraphs, string small)
+        {
+            var possiblyEmpty = paragraphs.Where(e => e.GetElementsByTagName("small").FirstOrDefault() != null).FirstOrDefault(e => e.GetElementsByTagName("small").FirstOrDefault().Text() == small)?.Text();
+            if (possiblyEmpty == "")
+                return null;
+            return possiblyEmpty;
+        }
+
+        private int TryGetParagraphValueInt(IHtmlCollection<IElement> paragraphs, string small)
+        {
+            var str = MatchOrNull(TryGetParagraphValueString(paragraphs, small), "[0-9]+")?.Value;
+            if (str == null || str == "")
+                return default;
+            return int.Parse(str);
+        }
+
+        private decimal TryGetParagraphValueDecimal(IHtmlCollection<IElement> paragraphs, string small)
+        {
+            var str = MatchOrNull(TryGetParagraphValueString(paragraphs, small), @"[0-9]+\.?[0-9]+")?.Value;
+            if (str == null || str == "")
+                return default;
+            return decimal.Parse(str);
+        }
+
+        private IDocument GetParsedHtmlFromUrl(string url)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
             var context = BrowsingContext.New(config);
-            return await context.OpenAsync(new Url(url));
+            return context.OpenAsync(new Url(url)).Result;
+        }
+
+        private Match MatchOrNull(string input, string rgx)
+        {
+            if (input != null)
+                return Regex.Match(input, rgx);
+            return null;
         }
     }
 }
